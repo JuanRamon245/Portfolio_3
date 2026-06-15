d293693255e5c8cac5802ed4822480b76642dc3d
JuanRamon245 <juanramondeleonmartin@gmail.com>
2026-06-15 21:18:58 +0200

    fire: borrado un par de archivos y bibliotecas innecesarias, aparte creado margen para los titulos

--- src/app/directives/animated-on-change-history.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimatedOnChangeHistory } from './animated-on-change-history';
-
-describe('AnimatedOnChangeHistory', () => {
-  it('should create an instance', () => {
-    const directive = new AnimatedOnChangeHistory();
-    expect(directive).toBeTruthy();
-  });
-});
